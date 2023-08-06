e1567ca02e64ad1eb871be80099df065ffa3dbbc
Хадков_Алексей <lhadkov@mail.ru>
2023-08-06 16:41:11 +0300

    add try except to init

--- src/video.py
+++ src/video.py
@@ -1,3 +1,4 @@
+import googleapiclient
 from googleapiclient.discovery import build
 import os
 
@@ -9,17 +10,26 @@ class Video:
         ''' Экземпляр инициализируется id видео. Дальше
          все данные будут подтягиваться по API. '''
         self.__video_id = video_id
+        self.__title = None
+        self.__url = None
+        self.__view_count = None
+        self.__like_count = None
+        self.__comment_count = None
         self.__youtube = self.get_service()
 
-        video_response = self.__youtube.videos().list(
-            part='snippet,statistics,contentDetails,topicDetails',
-            id=self.__video_id).execute()
+        try:
+            video_response = self.__youtube.videos().list(
+                part='snippet,statistics,contentDetails,topicDetails',
+                id=self.__video_id).execute()
 
-        self.__title = video_response['items'][0]['snippet']['title']
-        self.__url = "https://www.youtube.com/watch?v=" + self.__video_id
-        self.__view_count = video_response['items'][0]['statistics']['viewCount']
-        self.__like_count = video_response['items'][0]['statistics']['likeCount']
-        self.__comment_count = video_response['items'][0]['statistics']['commentCount']
+            self.__title = video_response['items'][0]['snippet']['title']
+            self.__url = "https://www.youtube.com/watch?v=" + self.__video_id
+            self.__view_count = video_response['items'][0]['statistics']['viewCount']
+            self.__like_count = video_response['items'][0]['statistics']['likeCount']
+            self.__comment_count = video_response['items'][0]['statistics']['commentCount']
+
+        except IndexError:
+            pass
 
     def __str__(self) -> str:
         ''' возвращает строку для пользователя '''
@@ -56,7 +66,7 @@ class Video:
         return self.__comment_count
 
     @classmethod
-    def get_service(cls):
+    def get_service(cls) -> googleapiclient.discovery.Resource:
         '''  возвращает объект для работы с YouTube API '''
         api_key: str = os.getenv('YT_API_KEY')
         return build('youtube', 'v3', developerKey=api_key)
